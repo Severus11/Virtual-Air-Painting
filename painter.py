bc080bba265f0a3afd486b4d871229da78d0fd34
Severus11 <parthsgupta@gmail.com>
2021-05-19 01:19:47 +0530

    added Finger Status

--- painter.py
+++ painter.py
@@ -22,7 +22,6 @@ default_overlay= overlay_image[0]
 
 detector= TH.handDetector(min_detection_confidence=.85)
 
-
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame,1)
@@ -30,9 +29,20 @@ while True:
 
     frame = detector.findHands(frame, draw=False)
     landmark_list = detector.findPosition(frame, draw =False)
+
     if(len(landmark_list)!=0):
         x1, y1 =(landmark_list[8][1:]) #index
         x2, y2 = landmark_list[12][1:] #middle    
+    
+    my_fingers = detector.fingerStatus()
+    print(my_fingers)
+    if (my_fingers[1]==1 and my_fingers[2] ==1):
+        cv2.putText(frame, 'Color Selector Mode', (900,680), fontFace=cv2.FONT_HERSHEY_COMPLEX, color= (0,255,255), thickness=3, fontScale=2)
+    
+    if (my_fingers[1]==1 and my_fingers[2] ==0):
+        cv2.putText(frame, "Writing Mode", (900,680), fontFace= cv2.FONT_HERSHEY_COMPLEX, color= (255,255,0), thickness= 3, fontScale=2)
+    
+
     cv2.imshow('paint', frame)
     cv2.waitKey(1)
 
--- track_hands.py
+++ track_hands.py
@@ -2,8 +2,6 @@ import cv2
 import time
 import mediapipe as mp 
 
-finger_tip_id= [4,8,12,16,20]
-
 class handDetector():
     def __init__(self, image_mode= False, max_num_hands =3, min_detection_confidence =0.5, min_tracking_confidence =0.5):
         self.image_mode = image_mode
@@ -14,6 +12,8 @@ class handDetector():
         self.mphands= mp.solutions.hands
         self.hands = self.mphands.Hands(self.image_mode, self.max_num_hands, self.min_detection_confidence, self.min_tracking_confidence)
         self.mpdraw = mp.solutions.drawing_utils 
+        self.finger_tip_id= [4,8,12,16,20]
+        
 
     def findHands(self, img, draw =True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -27,25 +27,35 @@ class handDetector():
         return img
 
     def findPosition(self, img, hand_num =0, draw = True):
-        lm_list=[]
+        self.lm_list=[]
         if self.results.multi_hand_landmarks:
             myHand= self.results.multi_hand_landmarks[hand_num]
             for id, lm in enumerate(myHand.landmark):
                 #print(id, lm)
                 h, w, c = img.shape
                 cx,cy = int(lm.x*w), int(lm.y*h)
-                lm_list.append([id, cx, cy])
+                self.lm_list.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, center=(cx,cy),radius=3, color=(255,255,255), thickness=1)
-        return lm_list
+        return self.lm_list
 
-    def fingerStatus(self, lm_list):
+    def fingerStatus(self):
+        #if len(self.lm_list)!=0:    
         fingers=[]
 
-        if (lm_list[finger_tip_id[0]][1]>lm_list[finger_tip_id[0]-1][1]):
+        if (self.lm_list[self.finger_tip_id[0]][1] < self.lm_list[self.finger_tip_id[0] - 1][1]):
             fingers.append(1)
         else:
             fingers.append(0)
+        
+        for i in range(1,5):
+            if (self.lm_list[self.finger_tip_id[i]][2] < self.lm_list[self.finger_tip_id[i] - 2][2]):
+                fingers.append(1)
+            else:
+                fingers.append(0)
+                
+        return fingers
+
 
 def main():
     cap = cv2.VideoCapture(0)
