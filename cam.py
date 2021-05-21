a2811b1fb2d30ec683a59be08483eedfe3414788
Severus11 <parthsgupta@gmail.com>
2021-05-21 22:01:43 +0530

    wip

--- cam.py
+++ cam.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import os 
 import track_hands as TH
 
@@ -6,17 +7,17 @@ header_img = "Images"
 header_img_list = os.listdir(header_img)
 
 class VideoCamera():
-    def __init__(self, xp=0, yp=0,overlay_image=[], 
-    draw_color =(255,200,100), x1=0, y1=0, x2=0, y2=0):
-        self.xp =xp
-        self.yp =yp
-        self.x1 = x1
-        self.y1 = y1
-        self.x2= x2
-        self.y2 = y2
+    def __init__(self,overlay_image=[],draw_color =(255,200,100)):
+        self.xp =0
+        self.yp =0
+        self.x1 = 0
+        self.y1 = 0
+        self.x2= 0
+        self.y2 = 0
         self.overlay_image = overlay_image  
         self.draw_color = draw_color
         self.detector = TH.handDetector(min_detection_confidence=0.85)
+        self.image_canvas = np.zeros((720,1280,3), np.uint8)
 
     def __del__(self):
         self.video.release()
@@ -28,13 +29,71 @@ class VideoCamera():
         
         return overlay_image
     
-    def next_step(self, overlay_image, )
+    def get_frame(self, frame, overlay_image):
+        
+        frame[0:125,0:1280] = self.default_overlay
+
+        frame = self.detector.findHands(frame, draw=True)
+        landmark_list = self.detector.findPosition(frame, draw =False)
+
+        if(len(landmark_list)!=0):
+            self.x1, self.y1 =(landmark_list[8][1:]) #index
+            self.x2, self.y2 = landmark_list[12][1:] #middle    
+        
+            my_fingers = self.detector.fingerStatus()
+            #print(my_fingers)
+            if (my_fingers[1]and my_fingers[2]):
+                self.xp, self.yp = 0,0
+                if (self.y1<125):
+                    if(200<self.x1<340):
+                        self.default_overlay = overlay_image[0] 
+                        self.draw_color = (255,0,0)
+                    elif (340<self.x1<500):
+                        self.default_overlay = overlay_image[1]
+                        self.draw_color = (47,225,245)
+                    elif (500<self.x1<640):
+                        self.default_overlay = overlay_image[2]
+                        self.draw_color = (197,47,245)
+                    elif (640<self.x1<780):
+                        self.default_overlay = overlay_image[3]
+                        self.draw_color = (53,245,47)
+                    elif (1100<self.x1<1280):
+                        self.default_overlay = overlay_image[4]
+                        self.draw_color = (0,0,0)
+
+                cv2.putText(frame, 'Color Selector Mode', (900,680), fontFace=cv2.FONT_HERSHEY_COMPLEX, color= (0,255,255), thickness=2, fontScale=1)
+                cv2.line(frame, (self.x1,self.y1), (self.x2,self.y2), color=self.draw_color, thickness=3)
+
+            if (my_fingers[1] and not my_fingers[2]):
+                        
+                cv2.putText(frame, "Writing Mode", (900,680), fontFace= cv2.FONT_HERSHEY_COMPLEX, color= (255,255,0), thickness=2, fontScale=1)
+                cv2.circle(frame, (self.x1,self.y1),15, self.draw_color, thickness=-1)
+
+                if self.xp ==0 and self.yp ==0:
+                    self.xp =self.x1 
+                    self.yp =self.y1
+                
+                if self.draw_color == (0,0,0):
+                    cv2.line(frame, (self.xp,self.yp),(self.x1,self.y1),color= self.draw_color, thickness=eraser_thickness)
+                    cv2.line(self.image_canvas, (self.xp,self.yp),(self.x1,self.y1),color= self.draw_color, thickness=eraser_thickness)
+
+                else:
+                    cv2.line(frame, (self.xp,self.yp),(self.x1,self.y1),color= self.draw_color, thickness=brush_thickness)
+                    cv2.line(self.image_canvas, (self.xp,self.yp),(self.x1,self.y1),color= self.draw_color, thickness=brush_thickness)
+                
+                self.xp , self.yp = self.x1, self.y1
+
+        img_gray = cv2.cvtColor(self.image_canvas, cv2.COLOR_BGR2GRAY)
+        _, imginv= cv2.threshold(img_gray, 50, 255, cv2.THRESH_BINARY_INV)
+        imginv = cv2.cvtColor(imginv, cv2.COLOR_GRAY2BGR)
+        frame = cv2.bitwise_and(frame, imginv)
+        frame =cv2.bitwise_or(frame, self.image_canvas)
 
 
-    
-    def conv(self,input_image):
-        frame = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-        return frame
+        return frame 
+
+
+
 
 def main():
     print('video')
